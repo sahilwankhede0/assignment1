158008e0eff07f86326550af4f6ac9f1056a803f
sahil <sahilwankhede0@gmail.com>
2022-05-05 18:55:18 +0530

    added twodice program

---
+++ twodice.sh
@@ -0,0 +1,8 @@
+#Add two Random Dice Number and Print the Result
+
+echo "Add two Random Dice Number and Print the Result"
+
+dice1=$((RANDOM%7))
+dice2=$((RANDOM%7))
+
+echo "dice1" $dice1 "dice2" $dice2
