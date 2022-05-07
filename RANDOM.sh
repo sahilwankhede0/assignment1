9c77907e71a66313493ff0e2ab7fd0441cbd6408
sahil <sahilwankhede0@gmail.com>
2022-05-07 13:42:18 +0530

    added to unit_conversion.sh file

--- RANDOM.sh
+++
@@ -1,5 +0,0 @@
-echo "Use Random Function (( RANDOM )) to get Single Digit"
-
-a=$((RANDOM%10))
-echo  "RANDOM NUMBER:" $a
-
--- unit_conversion.sh
+++ unit_conversion.sh
@@ -4,11 +4,14 @@ in=42
 feet=$((in/12))
 echo   "42 inches =" $feet "ft"
 
+
+
+#Rectangular Plot of 60 feet x 40 feet in meters"
 echo "Rectangular Plot of 60 feet x 40 feet in meters"
 a=$((40*60))
 b=0.3048
-meters=$((a*b))
-       echo $meters "meters"
+m=$(awk "BEGIN {print $a*$b}") 
+echo $m "meters"
 
 
 
