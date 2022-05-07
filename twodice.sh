c6b9213be5a8a64a2255b08ffcb7bc8cee982c80
sahil <sahilwankhede0@gmail.com>
2022-05-07 12:31:13 +0530

    some changes between twodice.sh

--- twodice.sh
+++ twodice.sh
@@ -2,7 +2,7 @@
 
 echo "Add two Random Dice Number and Print the Result"
 
-dice1=$((RANDOM%7))
-dice2=$((RANDOM%7))
+dice1=$((1+RANDOM%6))
+dice2=$((1+RANDOM%6))
 
 echo "dice1" $dice1 "dice2" $dice2
